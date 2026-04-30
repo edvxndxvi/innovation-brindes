265094b3d9214c4b365d434b44005b132b24d761
edvxndxvi <edvandavi1@hotmail.com>
2026-04-30 17:31:41 -0300

    fix: ajustando responsividade para mobile

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function LoginForm(){
     return (
         <div className="w-full flex items-center justify-center">
-            <form className="bg-lime-500 w-150 rounded-md p-8 pt-28 mb-4">
+            <form className="bg-lime-500 max-w-150 w-full rounded-md p-8 pt-28 mb-4">
                 <div className="max-w-sm mx-auto">
                     <div className="mb-6 flex flex-col gap-4">
                         <Input type="text" placeholder="Usuário" icon={User} name="usuario" id="usuario"/>
                         <Input type="password" placeholder="Senha" icon={Lock} name="senha" id="senha"/>
                     </div>
-                    <div className="flex items-center justify-between mb-8">
+                    <div className="flex flex-col sm:flex-row gap-4 items-center justify-between mb-8">
                         <div className="flex items-center">
                             <input type="checkbox" name="logado" id="logado" className="bg-transparent border border-white "/>
                             <label htmlFor="logado" className="ml-2 text-white">Manter logado</label>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,8 @@ export default function Login(){
     return (
         <div className="relative flex items-center justify-center h-screen bg-[url('@/public/login-bg.jpg')] bg-cover">
             <div className="absolute inset-0 bg-black/60 z-0"></div>
-            <div className="z-10 gap-8 flex flex-col items-center">
-                <h1 className="text-3xl font-bold text-lime-500 z-10">Bem-vindo a Innovation Brindes</h1>
+            <div className="z-10 gap-8 flex flex-col items-center px-4">
+                <h1 className="text-2xl sm:text-3xl font-bold text-lime-500 z-10 w-full text-center">Bem-vindo a Innovation Brindes</h1>
                 <LoginForm />
             </div>
         </div>
